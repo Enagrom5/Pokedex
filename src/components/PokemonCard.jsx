245e0577596e3dfddc28716e7f3ba5b14732cd99
MORGANE DEBARGE <morganedebarge@msn.com>
2023-10-10 09:51:20 +0200

    add dynamism from pokemonList

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,28 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
+  const pokemon = pokemonList[1].name;
   return (
-    <figure>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
-      <figcaption>Bulbasaur</figcaption>
-    </figure>
+    <p>
+      {pokemon === "bulbasaur" ? (
+        <figure>
+          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
+          <figcaption>Bulbasaur</figcaption>
+        </figure>
+      ) : (
+        "???"
+      )}{" "}
+    </p>
   );
 }
+
 export default PokemonCard;
